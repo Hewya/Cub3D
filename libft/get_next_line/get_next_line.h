754b9134a1b7d9c1017f71cdb9b5a309169a20e2
Gabarnou <146858389+Hewya@users.noreply.github.com>
2024-07-09 19:19:26 +0200

    gnl fixed

--- libft/get_next_line/get_next_line.h
+++ libft/get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: gabarnou <gabarnou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 12:20:06 by gabarnou          #+#    #+#             */
-/*   Updated: 2024/07/09 18:40:53 by gabarnou         ###   ########.fr       */
+/*   Updated: 2024/07/09 19:18:32 by gabarnou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "/home/gaelle/Documents/42/Cub3D/git_cub3d/libft/libft.h"
+# include "../libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
